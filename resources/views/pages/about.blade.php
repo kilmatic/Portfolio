9f547c0ff22e784d6d878ecbc17dc1c9081233f0
Khanya <kilakanya@gmail.com>
2018-06-10 20:42:20 +0200

    Beggan to build the About page...

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>This is the start of my change!!!</p>
+    <div class="jumbotron">
+        <div class="row-fluid">
+            <header class="span12 hero-unit">
+                <h1>{{$title}}</h1>
+                <p>This is the start of my change!!!</p>
+                <img style="width:25%" src="/storage/cover_image/70368_1528578597.jpg"><br><br>
+                <p><a href="#" class="btn btn-info">Learn more</a></p>
+            </header>
+        </div>
+    </div>
 @endsection('content')
